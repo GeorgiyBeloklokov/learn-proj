a807e5e7ae88a5ef132069b14095995d0419dadb
Georgiy Beloklokov <georgiy.reus@gmail.com>
2022-04-12 17:43:26 +0300

    init: implementing FC

--- module01/src/Components/Home/Home.tsx
+++ module01/src/Components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Grid, Typography } from '@mui/material';
 import CardPage from '../CardPage/CardPage';
 import { CharacterResponseType } from '../../Types/Types';
@@ -7,121 +7,107 @@ import { characterAPI } from '../ApiService/ApiService';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import axios from 'axios';
-
 export interface IHomeProps {
   setState: React.Dispatch<React.SetStateAction<IAppState>>;
   isSearchInputData: boolean;
 }
-export interface IHomeState {
-  newData: Array<CharacterResponseType>;
+/* export interface IHomeState {
+  allCardsData: Array<CharacterResponseType>;
   error: boolean;
   loading: boolean;
-}
+} */
+const Home: FC<IHomeProps> = (props) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [allCardsData, setAllCardsData] = useState<Array<CharacterResponseType>>([]);
+  const [isError, setIsError] = useState<boolean>(false);
 
-export default class Home extends React.Component<IHomeProps, IHomeState> {
-  constructor(props: IHomeProps) {
-    super(props);
-
-    this.state = {
-      newData: [],
-      error: false,
-      loading: true,
-    };
-  }
-
-  async componentDidMount() {
+  useEffect(() => {
+    secondFetchCards();
+    fetchCards();
+  }, [props.isSearchInputData]);
+  async function fetchCards() {
     try {
       const res = await characterAPI.getDefaultCharacter(); // for test:  const res = await axios.get('response');
-      this.setState((state) => {
-        return {
-          newData: [...state.newData, ...res], // for test: newData: [...state.newData, ...res.data],
-          loading: false,
-        };
-      });
-    } catch (error) {
-      this.setState({ error: true, loading: true });
-    }
-  }
-
-  async componentDidUpdate() {
-    try {
-      if (this.props.isSearchInputData) {
+      setAllCardsData([...allCardsData, ...res]); // for test: newData: [...state.newData, ...res.data],
+      setIsLoading(false);
+      console.log(`test1 isSearchInputData:`, props.isSearchInputData);
+      /* if (props.isSearchInputData) {
         const res = await characterAPI.getCharacter();
-
-        this.props.setState({ isSearchInputData: false });
-        this.setState((state) => {
-          return {
-            newData: [...res, ...state.newData],
-            loading: false,
-          };
-        });
-      }
+        props.setState({ isSearchInputData: false });
+        setAllCardsData([...res, ...allCardsData]);
+      } */
     } catch (error) {
-      this.props.setState({ isSearchInputData: false });
-      /* this.setState({ error: true, loading: true }); */
+      setIsError(true);
     }
   }
 
-  public render() {
-    return (
-      <Grid container spacing={3}>
-        <Typography
-          sx={{ position: 'absolute', ml: 95, mt: 3 }}
-          color="primary"
-          gutterBottom
-          variant="h6"
-          component="div"
-        >
-          Enter word &quot;Character&quot; in Search and press &quot;Enter&quot;
-        </Typography>
-        {this.state.loading && (
-          <>
-            <Box sx={{ display: 'flex', position: 'absolute', ml: 45, mt: 5 }}>
-              <CircularProgress />
-            </Box>
-            <Typography
-              sx={{ position: 'absolute', ml: 55, mt: 4 }}
-              color="error"
-              gutterBottom
-              variant="h4"
-              component="div"
-            >
-              Some error ocurred
-            </Typography>
-          </>
-        )}
-        {this.state.newData.map((item) => (
-          <Grid
-            /* data-testid="card-home" */
-            key={item.id}
-            sx={{ ml: 4, mt: 10, display: 'flex', justifyContent: 'space-between' }}
-            xs={12}
-            sm={6}
-            md={4}
-            lg={2}
-            item
-          >
-            <CardPage
-              key={item.id}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              status={item.status}
-              species={item.species}
-              type={item.type}
-              gender={item.gender}
-              origin={item.origin}
-              location={item.location}
-              url={item.url}
-              created={item.created}
-              setState={function (): void {
-                ('Function not implemented.');
-              }}
-              open={false}
-            />
-          </Grid>
-        ))}
-      </Grid>
-    );
+  async function secondFetchCards() {
+    console.log(`test2 isSearchInputData:`, props.isSearchInputData);
+    if (props.isSearchInputData) {
+      const res = await characterAPI.getCharacter();
+      props.setState({ isSearchInputData: false });
+      setAllCardsData([...res, ...allCardsData]);
+    }
   }
-}
+
+  return (
+    <Grid container spacing={3}>
+      <Typography
+        sx={{ position: 'absolute', ml: 95, mt: 3 }}
+        color="primary"
+        gutterBottom
+        variant="h6"
+        component="div"
+      >
+        Enter word &quot;Character&quot; in Search and press &quot;Enter&quot;
+      </Typography>
+      {isLoading && (
+        <>
+          <Box sx={{ display: 'flex', position: 'absolute', ml: 45, mt: 5 }}>
+            <CircularProgress />
+          </Box>
+          <Typography
+            sx={{ position: 'absolute', ml: 55, mt: 4 }}
+            color="error"
+            gutterBottom
+            variant="h4"
+            component="div"
+          >
+            Some error ocurred
+          </Typography>
+        </>
+      )}
+      {allCardsData.map((item) => (
+        <Grid
+          key={item.id}
+          sx={{ ml: 4, mt: 10, display: 'flex', justifyContent: 'space-between' }}
+          xs={12}
+          sm={6}
+          md={4}
+          lg={2}
+          item
+        >
+          <CardPage
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            status={item.status}
+            species={item.species}
+            type={item.type}
+            gender={item.gender}
+            origin={item.origin}
+            location={item.location}
+            url={item.url}
+            created={item.created}
+            setState={function (): void {
+              ('Function not implemented.');
+            }}
+            open={false}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+export default Home;
