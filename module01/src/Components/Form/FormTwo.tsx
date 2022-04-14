df8c96c8f90dfdc57f7ecb17acc86063cbbbcbb8
Georgiy Beloklokov <georgiy.reus@gmail.com>
2022-04-14 14:53:20 +0300

    feat: refactor form update 1

--- module01/src/Components/Form/FormTwo.tsx
+++ module01/src/Components/Form/FormTwo.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
-import { TextField, Box, Button } from '@mui/material';
+import { TextField, Box, Button, FormHelperText } from '@mui/material';
 import DatePicker from '@mui/lab/DatePicker';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
@@ -19,6 +19,7 @@ import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 export interface IHomeProps {
   setState: React.Dispatch<React.SetStateAction<IAppState>>;
   isSearchInputData: boolean;
+  error: boolean;
 }
 export interface IHomeState {
   allCardsData: Array<CharacterResponseType>;
@@ -30,6 +31,7 @@ interface IFormInput {
   muiDatePicker: Date | null;
   name: string;
   surname: string;
+  country: string;
 }
 
 /* Input = styled('input')({
@@ -41,7 +43,12 @@ const Form: FC<IHomeProps> = (props) => {
   const [isError, setIsError] = useState<boolean>(false); */
   const [state, setState] = useState<IFormInput[]>([]);
 
-  const { control, register, handleSubmit } = useForm<IFormInput>();
+  const {
+    control,
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInput>();
   console.log(state);
 
   const onSubmit: SubmitHandler<IFormInput> = (data: IFormInput) => {
@@ -56,8 +63,9 @@ const Form: FC<IHomeProps> = (props) => {
       >
         <div>
           <TextField
-            /* onFocus={() => this.setState({ isFormValid: true })} */
-            {...register('name', { required: true, maxLength: 20 })}
+            {...register('name', { required: 'Name is required', maxLength: 20 })}
+            error={Boolean(errors.name)}
+            helperText={errors.name?.message}
             type="name"
             name="name"
             id="outlined-required"
@@ -65,7 +73,9 @@ const Form: FC<IHomeProps> = (props) => {
             placeholder="Your Name.."
           />
           <TextField
-            {...register('surname', { required: true, pattern: /^[A-Za-z]+$/i })}
+            {...register('surname', { required: 'Surname is required' })}
+            error={Boolean(errors.surname)}
+            helperText={errors.surname?.message}
             type="surname"
             name="surname"
             id="outlined-disabled"
@@ -76,13 +86,20 @@ const Form: FC<IHomeProps> = (props) => {
             <Controller
               name="muiDatePicker"
               control={control}
-              defaultValue={new Date()}
-              rules={{ required: true }}
+              defaultValue={null}
+              rules={{ required: 'Date required' }}
               render={({ field }) => (
                 <DatePicker
                   label="Date input"
                   value={field.value}
-                  renderInput={(params) => <TextField name="muiDatePicker" {...params} />}
+                  renderInput={(params) => (
+                    <TextField
+                      name="muiDatePicker"
+                      {...params}
+                      error={Boolean(errors.muiDatePicker)}
+                      helperText={errors.muiDatePicker?.message}
+                    />
+                  )}
                   onChange={(e) => {
                     field.onChange(e);
                   }}
@@ -90,12 +107,13 @@ const Form: FC<IHomeProps> = (props) => {
               )}
             />
           </LocalizationProvider>
-          {/* <FormControl sx={{ mt: 2, ml: 2, width: '25ch' }}>
+          <FormControl sx={{ mt: 2, ml: 2, width: '25ch' }} error={Boolean(errors.country)}>
             <InputLabel id="demo-simple-select-label">Country</InputLabel>
             <Controller
-              name="muiDatePicker"
+              name="country"
               control={control}
-              defaultValue={new Date()}
+              defaultValue={''}
+              rules={{ required: 'Select required' }}
               render={({ field }) => (
                 <Select
                   labelId="demo-simple-select-label"
@@ -113,7 +131,8 @@ const Form: FC<IHomeProps> = (props) => {
                 </Select>
               )}
             />
-          </FormControl> */}
+            <FormHelperText>{errors.country?.message}</FormHelperText>
+          </FormControl>
           {/* <FormGroup>
             <FormControlLabel
               control={
