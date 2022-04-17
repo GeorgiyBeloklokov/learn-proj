29d56a3d5f788281270dcf8e0e4869e29c43f643
Georgiy Beloklokov <georgiy.reus@gmail.com>
2022-04-17 19:55:54 +0300

    feat: refactoring og index and axios.ts for deploy

--- module01/src/Components/__mocks__/axios.ts
+++ module01/src/Components/__mocks__/axios.ts
@@ -1,4 +1,6 @@
-const mockAxios = jest.genMockFromModule('axios');
+import axios from 'axios';
+
+const mockAxios = jest.genMockFromModule<typeof axios>('axios');
 // this is the key to fix the axios.create() undefined error!
 mockAxios.create = jest.fn(() => mockAxios);
 export default mockAxios;
